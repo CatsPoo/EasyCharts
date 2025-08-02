85d75968cccdde9a9b730e909b0e39db6acc5128
ron.gorlik <ron.gorlik@gmail.com>
2025-08-02 12:17:55 +0300

    init lines module

--- packages/apps/backend/src/lines/lines.module.ts
+++ packages/apps/backend/src/lines/lines.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { LinesController } from './lines.controller';
+import { LinessService } from './lines.service';
 
-@Module({})
+@Module({
+    imports:[],
+    controllers:[LinesController],
+    providers:[LinessService]
+})
 export class LinesModule {}
