8e91a17c9d81ccf34640af54b9b784989eca195e
ron.gorlik <ron.gorlik@gmail.com>
2025-08-02 19:11:08 +0300

    Feat - basic devices CRUD service

---
+++ packages/apps/backend/src/devices/dto/devices.dto.ts
@@ -0,0 +1,9 @@
+import { Device } from '@easy-charts/easycharts-types';
+
+export interface CreateDevicePayload {
+  device : Device
+}
+
+export interface UpdateDevicePayload{
+  device:Device
+}
--- packages/libs/easycharts-types/src/topology/Device.ts
+++ packages/libs/easycharts-types/src/topology/Device.ts
@@ -1,4 +1,4 @@
-import type { Position } from "@easy-charts/easycharts-types";
+import type { Position } from "../visualization/Position.js";
 import type { NamedTopologyEntity } from "./glabalTopology.js";
 export interface Device extends NamedTopologyEntity{
     type : string;
