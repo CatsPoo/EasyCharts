2ccc271661fb71849b8ab68c7fb80de223cb72be
ron.gorlik <ron.gorlik@gmail.com>
2025-08-09 16:31:17 +0300

    Fix - Merge vendors into devices modules

--- packages/apps/backend/src/devices/devices.module.ts
+++ packages/apps/backend/src/devices/devices.module.ts
@@ -3,13 +3,25 @@ import { DevicesController } from './devices.controller';
 import { DevicesService } from './devices.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DeviceEntity } from './entities/device.entity';
+import { VendorsController } from './vendors.controller';
+import { VendorsService } from './vendors.service';
+import { VendorEntity } from './entities/vendor.entity';
 
 @Module({
     imports:[
-        TypeOrmModule.forFeature([DeviceEntity]),
+        TypeOrmModule.forFeature([DeviceEntity,VendorEntity]),
     ],
-    controllers:[DevicesController],
-    providers:[DevicesService],
-    exports:[DevicesService]
+    controllers:[
+        DevicesController,
+        VendorsController
+    ],
+    providers:[
+        DevicesService,
+        VendorsService
+    ],
+    exports:[
+        DevicesService,
+        VendorsService
+    ]
 })
 export class DevicesModule {}
