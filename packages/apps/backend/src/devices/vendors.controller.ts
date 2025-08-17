d090f6e747ec7fcc3dc21f98462b9d0c2dd87caa
Ron Gorlik <ron.gorlik@gmail.com>
2025-08-17 21:44:50 +0300

    update vendors api to new format

--- packages/apps/backend/src/devices/vendors.controller.ts
+++ packages/apps/backend/src/devices/vendors.controller.ts
@@ -1,4 +1,4 @@
-import type { CreateVendorDto, UpdateVendorDto } from '@easy-charts/easycharts-types';
+import  { type VendorCreate, VendorCreateSchema,type  VendorUpdate, VendorUpdateSchema } from '@easy-charts/easycharts-types';
 import {
   Body,
   Controller,
@@ -11,17 +11,20 @@ import {
   Post,
   Put,
   Query,
+  UsePipes,
 } from '@nestjs/common';
 import { QueryDto } from '../query/dto/query.dto';
 import { VendorsService } from './vendors.service';
+import { ZodValidationPipe } from '../common/zodValidation.pipe';
 
 @Controller('vendors')
 export class VendorsController {
   constructor(private readonly vendorsService: VendorsService) {}
 
   @Post()
+  @UsePipes(new ZodValidationPipe(VendorCreateSchema))
   @HttpCode(HttpStatus.CREATED)
-  create(@Body() payload: CreateVendorDto) {
+  create(@Body() payload: VendorCreate) {
     return this.vendorsService.createVendor(payload);
   }
 
@@ -37,9 +40,10 @@ export class VendorsController {
   }
 
   @Put(':id')
+  @UsePipes(new ZodValidationPipe(VendorUpdateSchema))
   update(
     @Param('id', new ParseUUIDPipe()) id: string,
-    @Body() payload: UpdateVendorDto,
+    @Body() payload: VendorUpdate,
   ) {
     return this.vendorsService.updateVendor(id, payload);
   }
