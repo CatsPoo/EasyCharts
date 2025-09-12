a784c012328a0a770a60c164887421a873fa1a4d
Ron Gorlik <ron.gorlik@gmail.com>
2025-09-12 17:36:43 +0300

    fix imports

--- packages/apps/frontend/src/components/ChartEditor/ChartEditor.tsx
+++ packages/apps/frontend/src/components/ChartEditor/ChartEditor.tsx
@@ -24,7 +24,7 @@ import ReactFlow, {
 import "reactflow/dist/style.css";
 import { useListAssets } from "../../hooks/assetsHooks";
 import { DevicesSidebar } from "../ChartsViewer/DevicesSideBar";
-import DeviceNode from "./DeviceNode";
+import DeviceNode from "../DeviceNode/DeviceNode";
 import type { DeviceNodeData } from "../DeviceNode/interfaces/deviceModes.interfaces";
 import MenuList from "./MenueList";
 import { useThemeMode } from "../../contexts/ThemeModeContext";
