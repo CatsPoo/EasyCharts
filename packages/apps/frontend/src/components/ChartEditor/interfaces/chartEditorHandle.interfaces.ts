60010d8ad23e653c8c114987b0dda369b3888d5e
Ron Gorlik <ron.gorlik@gmail.com>
2025-09-13 14:10:44 +0300

    fix return type

--- packages/apps/frontend/src/components/ChartEditor/interfaces/chartEditorHandle.interfaces.ts
+++ packages/apps/frontend/src/components/ChartEditor/interfaces/chartEditorHandle.interfaces.ts
@@ -2,6 +2,6 @@ import type { Chart } from "@easy-charts/easycharts-types";
 import type { MouseEvent } from "react";
 
 export interface ChartEditorHandle  {
-  onSave: (e?:MouseEvent<HTMLButtonElement>) => Promise<Chart>;
+  onSave: (e?:MouseEvent<HTMLButtonElement>) => Promise<Chart|null>;
   isChangesMade: () => boolean;
 };
