2a9633e5eb749cc631b2a240aae58417f2652266
Ron Gorlik <ron.gorlik@gmail.com>
2025-09-17 20:51:19 +0300

    Add orphand lines cleanup

--- packages/apps/backend/src/lines/lines.service.ts
+++ packages/apps/backend/src/lines/lines.service.ts
@@ -3,31 +3,51 @@ import { LineEntity } from './entities/line.entity';
 import { Line } from '@easy-charts/easycharts-types';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { LineOnChartEntity } from '../charts/entities/lineonChart.emtity';
 
 @Injectable()
 export class LinessService {
+  constructor(
+    @InjectRepository(LineEntity)
+    private readonly linesrepo: Repository<LineEntity>
+  ) {}
+  convertLineEntity(lineEntity: LineEntity): Line {
+    const { id, sourcePort, targetPort } = lineEntity;
+    return {
+      id,
+      sourcePort,
+      targetPort,
+    } as Line;
+  }
 
-    constructor(
-        @InjectRepository(LineEntity)
-        private readonly linesrepo: Repository<LineEntity>,
-    ) {}
-    convertLineEntity(lineEntity: LineEntity): Line {
-      const { id, sourcePort, targetPort } = lineEntity;
-      return {
-        id,
-        sourcePort,
-        targetPort,
-      } as Line;
-    }
+  convertLineToEntity(line: Line): LineEntity {
+    const { id, sourcePort, targetPort } = line;
+    return {
+      id,
+      sourcePort,
+      sourcePortId: sourcePort.id,
+      targetPortId: targetPort.id,
+      targetPort,
+    } as LineEntity;
+  }
 
-    convertLineToEntity(line:Line) :LineEntity {
-      const {id,sourcePort,targetPort} = line
-      return {
-        id,
-        sourcePort,
-        sourcePortId:sourcePort.id,
-        targetPortId:targetPort.id,
-        targetPort
-      } as LineEntity
-    }
+  async deleteOrphanLines(): Promise<number> {
+    return this.linesrepo.manager.transaction(async (m) => {
+      const usedLinesSub = m
+        .createQueryBuilder()
+        .subQuery()
+        .select("loc.lineId")
+        .from(LineOnChartEntity, "loc")
+        .getQuery(); // -> (SELECT loc.lineId FROM lines_on_chart loc)
+
+      const res = await m
+        .createQueryBuilder()
+        .delete()
+        .from(LineEntity)
+        .where(`id NOT IN ${usedLinesSub}`)
+        .execute();
+
+      return res.affected ?? 0;
+    });
+  }
 }
