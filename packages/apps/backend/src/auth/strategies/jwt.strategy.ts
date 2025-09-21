507bbb4eb51e10052ee7e2217a4efe36def9ed14
Ron Gorlik <ron.gorlik@gmail.com>
2025-09-21 21:09:41 +0300

    fix name mispelling

--- packages/apps/backend/src/auth/strategies/jwt.strategy.ts
+++ packages/apps/backend/src/auth/strategies/jwt.strategy.ts
@@ -1,22 +1,21 @@
 import { PassportStrategy } from "@nestjs/passport";
-import {Strategy,ExtractJwt} from 'passport-jwt'
+import { Strategy, ExtractJwt, StrategyOptions } from "passport-jwt";
 import { AppConfigService } from "../../appConfig/appConfig.service";
 import { AuthJwtPayload } from "../interfaces/jwt.interfaces";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy){
-    constructor(
-      appConfigService:AppConfigService
-    ){
-        super({
-          jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-          secretOrKey: appConfigService.getConfig().jwt.secret,
-          ignoreExperation:false
-        });
-    }
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(appConfigService: AppConfigService) {
+    const strategyOptions: StrategyOptions = {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: appConfigService.getConfig().jwt.secret,
+      ignoreExpiration: false,
+    };
+    super(strategyOptions);
+  }
 
-    validate(payload: AuthJwtPayload) : string {
+  validate(payload: AuthJwtPayload): string {
     return payload.sub;
   }
 }
