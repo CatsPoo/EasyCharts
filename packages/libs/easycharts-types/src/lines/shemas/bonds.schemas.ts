c138729ecba1e775d7df5d96ee3406f95d816561
Ron Gorlik <ron.gorlik@gmail.com>
2025-10-02 17:28:37 +0300

    Add bond type and entity

---
+++ packages/libs/easycharts-types/src/lines/shemas/bonds.schemas.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const BondCreateSchema = z.object({
+  id: z.string().uuid().optional(),
+  name: z.string(),
+  membersLines:z.array(z.string().uuid())
+});
+
+export const BondUpdateSchema = BondCreateSchema.partial();
+
+export const BondSchema = BondCreateSchema
