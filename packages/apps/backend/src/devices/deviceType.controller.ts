b3c949e9188957f59a2b34a4ebfcfec295d13819
einav <daganeinav@gmail.com>
2025-10-02 19:45:25 +0300

    created service, updated controller

---
+++ packages/apps/backend/src/devices/deviceType.controller.ts
@@ -0,0 +1,60 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UseGuards
+} from "@nestjs/common";
+import { ZodValidationPipe } from "../common/zodValidation.pipe";
+import { DeviceTypeService } from "./deviceType.service";
+import { JwdAuthGuard } from "../auth/guards/jwtAuth.guard";
+import { PermissionsGuard } from "../auth/guards/permissions.guard";
+import { RequirePermissions } from "../auth/decorators/permissions.decorator";
+import { type DeviceTypeCreate, DeviceTypeCreateSchema, type DeviceTypeUpdate, DeviceTypeUpdateSchema, Permission } from "@easy-charts/easycharts-types";
+@UseGuards(JwdAuthGuard,PermissionsGuard)
+@Controller("deviceType")
+export class DeviceTypeController {
+  constructor(private readonly DeviceTypeService: DeviceTypeService) {}
+
+  @RequirePermissions(Permission.ASSET_CREATE)
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(
+    @Body(new ZodValidationPipe(DeviceTypeCreateSchema)) payload: DeviceTypeCreate) {
+    return this.DeviceTypeService.createDeviceType(payload);
+  }
+
+//   @RequirePermissions(Permission.ASSET_READ)
+//   @Get()
+//   list(@Query() q: ListDeviceTypesQueryDto) {
+//     return this.DeviceTypeService.listDeviceTypes(q);
+//   }
+
+  @RequirePermissions(Permission.ASSET_READ)
+  @Get(":id")
+  getById(@Param("id", new ParseUUIDPipe()) id: string) {
+    return this.DeviceTypeService.getDeviceTypeById(id);
+  }
+
+  @RequirePermissions(Permission.ASSET_EDIT)
+  @Put(":id")
+  update(
+    @Param("id", new ParseUUIDPipe()) id: string,
+    @Body(new ZodValidationPipe(DeviceTypeUpdateSchema)) payload: DeviceTypeUpdate
+  ) {
+    return this.DeviceTypeService.updateDeviceType(id, payload);
+  }
+
+  @RequirePermissions(Permission.ASSET_DELETE)
+  @Delete(":id")
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param("id", new ParseUUIDPipe()) id: string) {
+    return this.DeviceTypeService.removeDeviceType(id);
+  }
+}
