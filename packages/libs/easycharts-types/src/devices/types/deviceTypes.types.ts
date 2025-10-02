74c1d4896c2312931a59a664c47bc97cd6bbb08f
einav <daganeinav@gmail.com>
2025-10-02 19:35:30 +0300

    updated deviceTypes.types.ts

--- packages/libs/easycharts-types/src/devices/types/deviceTypes.types.ts
+++ packages/libs/easycharts-types/src/devices/types/deviceTypes.types.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-import { DeviceTypeCreateSchema, DeviceTypeSchema, DeviceTypeUpdateSchema } from "../schemas/deviceType.schemas.js";
+import { DeviceTypeCreateSchema, DeviceTypeSchema, DeviceTypeUpdateSchema } from "../schemas/deviceTypes.schemas.js";
 
 export type DeviceTypeCreate = z.infer<typeof DeviceTypeCreateSchema>;
 export type DeviceTypeUpdate = z.infer<typeof DeviceTypeUpdateSchema>;
