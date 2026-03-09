c1318cc548bf02b5b9153f375079049220b9477c
Ron Gorlik <ron.gorlik@gmail.com>
2026-03-09 18:39:15 +0200

    added - default cloud image

--- packages/apps/backend/src/overlayElements/overlayElements.seeder.ts
+++ packages/apps/backend/src/overlayElements/overlayElements.seeder.ts
@@ -5,15 +5,7 @@ import { OverlayElementEntity } from '../devices/entities/overlayElement.entity'
 import { UsersService } from '../auth/user.service';
 
 /** Inline SVG data-URL for a generic cloud icon used as the default thumbnail. */
-const CLOUD_IMAGE_URL =
-  'data:image/svg+xml;utf8,' +
-  encodeURIComponent(
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%236366f1">' +
-    '<path d="M19.35 10.04A7.49 7.49 0 0 0 12 4C9.11 4 6.6 5.64 5.35 8.04' +
-    'A5.994 5.994 0 0 0 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5' +
-    ' 0-2.64-2.05-4.78-4.65-4.96z"/>' +
-    '</svg>',
-  );
+const CLOUD_IMAGE_URL = 'unnamed.jpg'
 
 @Injectable()
 export class OverlayElementsSeeder implements OnApplicationBootstrap {
